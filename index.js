f2bc97d6ac5add3b40fbd9ac42f9bf43de018461
deandenaki <sonicmailfaster@gmail.com>
2023-09-15 09:58:08 +0700

    ver 1.1

---
+++ index.js
@@ -0,0 +1,23 @@
+/* conosle.log untuk menampilkan sebuah data, tapi tida di tampilkan 
+di halaman website, melainkan harus membuka inspect element */
+
+//document.write ("saya sedang belajar cuy")
+
+/* document write untuk menampilkan sebuah data dan di tampilkan 
+di halaman website, tapi tidak bisa membuat style seperti di html */
+
+//console.log ("saya sedang ngotak ngatik")
+
+//cara menambahkan text dengan id
+//document.getElementById ('judul').innerHTML = 'wow this is real'
+
+//cara menghitung dengan id
+//document.getElementById ('judul').innerHTML = 4+5
+
+//cara menampilkan data tanggal sekarang
+//document.getElementById ('judul').innerHTML = Date();
+
+//cara menampilkan window
+//window.alert ('please **** me');
+
+//console.log (Date())
